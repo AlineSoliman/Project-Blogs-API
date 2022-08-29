1075159d1cee7d75300cf552af53f5ea2f291ff8
Aline Soliman <alineapreis@hotmail.com>
2022-08-29 18:01:30 -0300

    feat: cria model blogpost

---
+++ src/database/models/blogpost.js
@@ -0,0 +1,22 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+  },  {
+    createdAt: 'published',
+    updatedAt: 'updated',
+    tableName:'BlogPosts',
+    underscored: true
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      as: "user",
+      foreignKey: "userId"
+    });
+  }
+  return BlogPost;
+};
+
+module.exports = BlogPost;
