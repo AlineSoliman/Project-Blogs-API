6270baa906476115980e14922a11c8a6be56141b
Aline Soliman <alineapreis@hotmail.com>
2022-08-29 18:58:20 -0300

    feat: insere ID nas models

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,6 +1,7 @@
 const Category = (sequelize, DataTypes) => {
   const Category = sequelize.define("Category", {
-    name: DataTypes.STRING,
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING
   });
 
   return Category;
