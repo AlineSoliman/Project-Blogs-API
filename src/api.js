c4c883e1322f64625120c0c0364291463fc3b67f
Aline Soliman <alineapreis@hotmail.com>
2022-08-31 18:07:31 -0300

    feat: insere o endpoint POST /login no arquivo api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const userController = require('./controllers/userController');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/login', userController);
 
 // ...  
 
