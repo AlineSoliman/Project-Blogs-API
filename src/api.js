8932a5b622862705a2b90da2943b60053b23eef6
Aline Soliman <alineapreis@hotmail.com>
2022-09-01 17:56:00 -0300

    feat: altera rota da pasta api para server

--- src/api.js
+++ src/api.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const userController = require('./controllers/userController');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/login', userController);
 
 // ...  
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 const app = require('./api');
+require('express-async-errors');
+const { login, createUserController } = require('./controllers/userController');
+
+app.post('/login', login);
+
+app.post('/user', createUserController);
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -7,6 +13,12 @@ const port = process.env.API_PORT || 3000;
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  const [code, message] = err.message.split('|');
+    return res.status(code).json({ message });
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
